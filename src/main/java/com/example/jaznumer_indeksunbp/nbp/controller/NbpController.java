e58b3e0fa08d362f47001a529ec3d209acfdbcbb
Łukasz Macoń <luk.macon@gmail.com>
2021-07-04 15:30:59 +0200

    Add screen shot and swagger documentation

--- src/main/java/com/example/jaznumer_indeksunbp/nbp/controller/NbpController.java
+++ src/main/java/com/example/jaznumer_indeksunbp/nbp/controller/NbpController.java
@@ -2,7 +2,10 @@ package com.example.jaznumer_indeksunbp.nbp.controller;
 
 import com.example.jaznumer_indeksunbp.nbp.model.NbpResponse;
 import com.example.jaznumer_indeksunbp.nbp.service.NbpService;
+import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,8 +19,30 @@ public class NbpController {
         this.nbpService = nbpService;
     }
 
+
+    @ApiOperation(value = "Get average price for given date ranges",
+            response = NbpResponse.class,
+            notes = "this method will return NbpResponse id, average price, timeOfRequest or HTTP 404 if not found")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "OK"),
+            @ApiResponse(code = 404, message = "Nbp api not found"),
+            @ApiResponse(code = 500, message = "Unexpected Error"),
+    })
     @GetMapping(value = "/calculate", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<NbpResponse> calculateRootForCurrency(@RequestParam(defaultValue = "2013-01-31") String startDate, @RequestParam(defaultValue = "2013-01-31") String endDate){
+    public ResponseEntity<NbpResponse> calculateRootForCurrency(
+            @ApiParam(name ="startDate",
+                    type="string",
+                    value= "date",
+                    example = "2013-01-31",
+                    required = true)
+            @RequestParam(defaultValue = "2013-01-31") String startDate,
+
+            @ApiParam(name ="endDate",
+                    type="string",
+                    value= "date",
+                    example = "2013-01-31",
+                    required = true)
+            @RequestParam(defaultValue = "2013-01-31") String endDate){
         return ResponseEntity.ok(nbpService.calculateRootForCurrency(startDate, endDate));
     }
 }
--- src/main/java/com/example/jaznumer_indeksunbp/nbp/model/NbpResponse.java
+++ src/main/java/com/example/jaznumer_indeksunbp/nbp/model/NbpResponse.java
@@ -1,40 +1,31 @@
 package com.example.jaznumer_indeksunbp.nbp.model;
 
+import io.swagger.annotations.ApiModelProperty;
+
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
 public class NbpResponse {
+    @ApiModelProperty(notes="This is id of the NbpResponse", required = true, value = "some id", example = "1")
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-
-
+    @ApiModelProperty(notes="This is average for currency in time period of the NbpResponse", required = true, value = "some double", example = "5.1")
     private double average;
+
+    @ApiModelProperty(notes="This is Enum Gold of the NbpResponse", required = true, value = "some enum", example = "GOLD")
     @Enumerated(EnumType.STRING)
     private GoldEnumCategory category;
 
+    @ApiModelProperty(notes="This is start date of the NbpResponse", required = true, value = "some String", example = "2021-01-30")
     private String startDate;
 
-    public String getStartDate() {
-        return startDate;
-    }
-
-    public void setStartDate(String startDate) {
-        this.startDate = startDate;
-    }
-
-    public String getEndDate() {
-        return endDate;
-    }
-
-    public void setEndDate(String endDate) {
-        this.endDate = endDate;
-    }
-
+    @ApiModelProperty(notes="This is end date of the NbpResponse", required = true, value = "some String", example = "2021-01-30")
     private String endDate;
 
+    @ApiModelProperty(notes="This is time for request of the the NbpResponse", required = true, value = "some LocalDayTime", example = "2021-07-04 01:01:47.696179")
     private LocalDateTime createdAt;
 
     public NbpResponse() {
@@ -73,5 +64,19 @@ public class NbpResponse {
         this.createdAt = createdAt;
     }
 
+    public String getStartDate() {
+        return startDate;
+    }
 
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
 }
