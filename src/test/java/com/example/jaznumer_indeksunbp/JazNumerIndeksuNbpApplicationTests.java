843dba4eba844079a98c166fd1f3ed39fe170c35
Łukasz Macoń <luk.macon@gmail.com>
2021-07-04 14:07:40 +0200

    Create project

---
+++ src/test/java/com/example/jaznumer_indeksunbp/JazNumerIndeksuNbpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jaznumer_indeksunbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JazNumerIndeksuNbpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
